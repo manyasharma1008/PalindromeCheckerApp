575d45c743c7da960be88cd3b2f043c4eab54c00
manyasharma1008 <manya.s1008@gmail.com>
2026-02-12 16:06:51 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+/*
+Use Case 1: Palindrome Checker App
+Author: Manya Sharma
+Date: 12-02-2025
+Reg. No.: RA2411026010261
+*/
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System.");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
