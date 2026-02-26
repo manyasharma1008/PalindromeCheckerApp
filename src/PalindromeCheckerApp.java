1f34f2f8a5727a88fed7cdd11b7e42129cb5cfa6
Manya Sharma <manya.s1008@gmail.com>
2026-02-26 16:22:30 +0530

    Manya Sharma Added UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,29 +4,24 @@ Author: Manya Sharma
 Date: 12-02-2025
 Reg. No.: RA2411026010261
 */
-
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        String check = "Madam".toLowerCase();
+        Scanner scanner = new Scanner(System.in);
 
-        int i = 0;
-        int j = check.length() - 1;
-        boolean isPalindrome = true;
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
 
-        while(i < j) {
-            if(check.charAt(i) != check.charAt(j)) {
-                isPalindrome = false;
-                break;
-            }
-            i++;
-            j--;
-        }
+        StringBuilder sb = new StringBuilder(original);
+        String reversed = sb.reverse().toString();
 
-        if(isPalindrome) {
-            System.out.println("Palindrome");
+        if (original.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
         } else {
-            System.out.println("Not palindrome");
+            System.out.println("The string is NOT a palindrome.");
         }
+
+        scanner.close();
     }
 }
