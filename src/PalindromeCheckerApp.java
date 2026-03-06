ce6ca9fd6ecd961b670822351c6d6f8172e90fa2
manyasharma1008 <manya.s1008@gmail.com>
2026-03-06 15:05:08 +0530

    Manya Sharma Added UC13

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,68 +1,69 @@
-import java.util.Scanner;
 import java.util.Stack;
 import java.util.Deque;
 import java.util.ArrayDeque;
+import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    interface PalindromeStrategy {
-        boolean check(String input);
-    }
-
-    static class StackStrategy implements PalindromeStrategy {
-        public boolean check(String input) {
-            Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < input.length(); i++) {
-                stack.push(input.charAt(i));
-            }
-
-            String reversed = "";
-            while (!stack.isEmpty()) {
-                reversed += stack.pop();
-            }
-
-            return input.equals(reversed);
+    public static boolean stackPalindrome(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
         }
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+        return input.equals(reversed);
     }
 
-    static class DequeStrategy implements PalindromeStrategy {
-        public boolean check(String input) {
-            Deque<Character> deque = new ArrayDeque<>();
-            for (int i = 0; i < input.length(); i++) {
-                deque.addLast(input.charAt(i));
+    public static boolean dequePalindrome(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
             }
+        }
+        return true;
+    }
 
-            while (deque.size() > 1) {
-                if (deque.removeFirst() != deque.removeLast()) {
-                    return false;
-                }
-            }
-
+    public static boolean recursivePalindrome(String input, int start, int end) {
+        if (start >= end)
             return true;
-        }
+        if (input.charAt(start) != input.charAt(end))
+            return false;
+        return recursivePalindrome(input, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a string to check palindrome:");
+        System.out.println("Enter a string to check palindrome performance:");
         String input = scanner.nextLine();
 
-        System.out.println("Choose strategy: 1 = Stack, 2 = Deque");
-        int choice = scanner.nextInt();
-        scanner.nextLine(); // consume newline
+        long startTime, endTime;
 
-        PalindromeStrategy strategy;
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
+        startTime = System.nanoTime();
+        boolean stackResult = stackPalindrome(input);
+        endTime = System.nanoTime();
+        long stackTime = endTime - startTime;
 
-        if (strategy.check(input)) {
-            System.out.println("The string \"" + input + "\" is a Palindrome.");
-        } else {
-            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
-        }
+        startTime = System.nanoTime();
+        boolean dequeResult = dequePalindrome(input);
+        endTime = System.nanoTime();
+        long dequeTime = endTime - startTime;
+
+        startTime = System.nanoTime();
+        boolean recursiveResult = recursivePalindrome(input, 0, input.length() - 1);
+        endTime = System.nanoTime();
+        long recursiveTime = endTime - startTime;
+
+        System.out.println("\nResults:");
+        System.out.println("Stack method: " + stackResult + ", Time: " + stackTime + " ns");
+        System.out.println("Deque method: " + dequeResult + ", Time: " + dequeTime + " ns");
+        System.out.println("Recursive method: " + recursiveResult + ", Time: " + recursiveTime + " ns");
 
         scanner.close();
     }
